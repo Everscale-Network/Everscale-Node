54fff787aaef3d6fd34c3a27653bcb5a48fc7478
Ilyar <ilyar.software@gmail.com>
2020-08-09 21:12:21 +0300

    fix script for format

--- run-clang-format.sh
+++ run-clang-format.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+set -o errexit
+
+if ! [ -x "$(command -v clang-format)" ]; then
+  echo "clang-format is not installed"
+  exit 1
+fi
+
 ./src.sh | xargs -n 1 clang-format -verbose -style=file -i
